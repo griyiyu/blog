b27f87e66bfa03f9d11e9162f4cb5b173d08e3f6
grigit <ariasgri@gmail.com>
2014-07-12 19:05:46 -0300

    Agregado del modelo user y los controllers para la sesion de usuario

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140712191552) do
+ActiveRecord::Schema.define(version: 20140712220108) do
 
   create_table "comments", force: true do |t|
     t.string   "author"
@@ -26,6 +26,13 @@ ActiveRecord::Schema.define(version: 20140712191552) do
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password_hash"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
