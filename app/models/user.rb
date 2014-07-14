386366d4fc5b173433508a78c3e3011ee70a5ef8
Gri <ariasgri@gmail.com>
2014-07-14 15:28:02 -0300

    Creación del authenticate y encrypt_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,26 @@
 class User < ActiveRecord::Base
+  attr_accessible :email, :password_hash
+  attr_accessor :password
   
-  def self.authenticate(user_name, password)
-    where(user_name: user_name, password: password).first
+  before_save :encrypt_password
+  
+  validates_presence_of :password_hash, :on => :create
+  validates_presence_of :email, :on => :create
+  validates_uniqueness_of :email
+  
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == Digest::MD5.digest(user_params[:password_hash]).force_encoding("utf-8") 
+      user
+    else
+      nil
+    end
+  end
+
+  def encrypt_password
+    if password.present?
+      self.password_hash = Digest::MD5.digest(user_params[:password_hash]).force_encoding("utf-8") 
+    end
   end
   
 end
