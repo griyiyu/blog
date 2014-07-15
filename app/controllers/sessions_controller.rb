30e28811eed43e3839d7e512cba863d4da8ebdff
grigit <ariasgri@gmail.com>
2014-07-15 00:34:48 -0300

    Agregado del email y logout en la aplicación

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     @current_user = User.authenticate(user_params[:email], user_params[:password_hash])
     if @current_user 
       session[:current_user_id] = @current_user.id
+      session[:current_user_email] = @current_user.email
       flash[:notice] = 'Welcome.'
       redirect_to posts_path
     else
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
  
   def destroy
-    @current_user = session[:current_user_id] = nil
+    @current_user = session[:current_user_id] = session[:current_user_email] = nil
     redirect_to signin_path
   end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,8 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-    # Encrypta la password.
-    #@user.password_hash = Digest::MD5.digest(user_params[:password_hash]).force_encoding("utf-8") 
     @user.save
     redirect_to users_path
-    
   end
   
   
